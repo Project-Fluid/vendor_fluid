67a2eb49ffdee7495271e5c04338934e0c9a4346
Michael Bestas <mkbestas@lineageos.org>
2018-03-21 18:29:45 +0200

    AOSDP: Set TARGET_CRYPTFS_HW_PATH if HW crypto is enabled
    * So TWRP can use it
    
    Change-Id: Ibc1d9d943d6d4e4e5248f623cd06782335f8ae98
    Signed-off-by: Thespartann <danilastefan69@yahoo.ro>

--- config/BoardConfigAOSDP.mk
+++ config/BoardConfigAOSDP.mk
@@ -1,3 +1,8 @@
+# QCOM HW crypto
+ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
+    TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
+endif
+
 include vendor/aosdp/config/BoardConfigKernel.mk
 
 include vendor/aosdp/config/BoardConfigSoong.mk
