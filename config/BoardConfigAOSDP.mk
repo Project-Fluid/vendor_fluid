260ef2e7826a81a15eb47a1ad6b15cf2ab4d4d7c
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-25 11:10:25 -0700

    AOSDP: Move QC board variables earlier
    These variables are usually set in a device's BoardConfig, setting them
    in qcom_target is at the very end of the "configuration process" which
    results in them being unavailable to plenty of other configuration
    "things" (ex. soong namespaces or soong config). Move them to right
    after a device's BoardConfig has been found and loaded.
    
    Change-Id: Iddd731202d22ed3f8eb010197ce20d3c75a1f40a
    Signed-off-by: Thespartann <danilastefan69@yahoo.ro>

--- config/BoardConfigAOSDP.mk
+++ config/BoardConfigAOSDP.mk
@@ -1,6 +1,5 @@
-# QCOM HW crypto
-ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
-    TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+include vendor/aosdp/config/BoardConfigQcom.mk
 endif
 
 include vendor/aosdp/config/BoardConfigKernel.mk
