1f240c6942808a0a1dd745ee609291a8ae93c7d3
Thespartann <danilastefan69@yahoo.ro>
2019-02-22 20:46:19 +0200

    vendor: Import APNs from LineageOS
    <!-- Credits to LineageOS team -->
    
    Reference:
    
    prebuilt/common/Android.mk -- Left at commit https://github.com/LineageOS/android_vendor_lineage/commit/0d2fa55f2f88e73018a749a3fe34f33ec365fbbc
    prebuilt/common/etc/apns-conf.xml -- Left at commit https://github.com/LineageOS/android_vendor_lineage/commit/42252b823b187e8c363723108e16f288fd3fc1be
    prebuilt/common/etc/sensitive_pn.xml -- Left at commit https://github.com/LineageOS/android_vendor_lineage/commit/ab6ec3af078711d2b69def41dc1399aace198da4
    tools/custom_apns.py -- Left at commit https://github.com/LineageOS/android_vendor_lineage/commit/0d2fa55f2f88e73018a749a3fe34f33ec365fbbc
    
    Signed-off-by: Thespartann <danilastefan69@yahoo.ro>

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,14 +1,14 @@
 # Inherit common stuff
 $(call inherit-product, vendor/aosdp/config/common.mk)
+
+# Sensitive Phone Numbers list
+PRODUCT_COPY_FILES += \
+    vendor/aosdp/prebuilt/common/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
+
 # World APN list
-PRODUCT_COPY_FILES += \
-    vendor/aosdp/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
-# World SPN overrides list
-PRODUCT_COPY_FILES += \
-    vendor/aosdp/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
-# Selective SPN list for operator number who has the problem.
-PRODUCT_COPY_FILES += \
-    vendor/aosdp/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+PRODUCT_PACKAGES += \
+    apns-conf.xml
+
 # SIM Toolkit
 PRODUCT_PACKAGES += \
     Stk
