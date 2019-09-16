0b74c51a59a7265dfe28e3661702c82c16447f2e
harsh-23 <harsh.v23labs@gmail.com>
2019-09-16 10:41:11 +0200

    ColorBucket: add a few new dark theme shades [3/3]
    - color hex's from SwiftInstaller app default backgrounds
    
    Change-Id: I98283c4621f89f3a3f2ba7ee48a6815e6e5b2d97
    Signed-off-by: harsh-23 <harsh.v23labs@gmail.com>
    Signed-off-by: Akito Mizukito <akito@evolution-x.org>

--- config/packages.mk
+++ config/packages.mk
@@ -1,3 +1,14 @@
 # Custom Fluid packages
 PRODUCT_PACKAGES += \
     StitchImage
+
+# Custom Overlays
+# Settings
+PRODUCT_PACKAGES += \
+    SystemPitchBlackOverlay \
+    SystemDarkGrayOverlay \
+    SystemUIDarkGrayOverlay \
+    SystemStyleOverlay \
+    SystemUIStyleOverlay \
+    SystemNightOverlay \
+    SystemUINightOverlay
