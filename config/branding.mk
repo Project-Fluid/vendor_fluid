6cc084e7948f1f3c60da210ed020afee0cb53150
ender1324 <ender1324@abv.bg>
2020-08-27 17:55:11 +0530

    vendor: Fluid versioning

--- config/branding.mk
+++ config/branding.mk
@@ -7,18 +7,22 @@ CUSTOM_DATE_DAY := $(shell date -u +%d)
 CUSTOM_DATE_HOUR := $(shell date -u +%H)
 CUSTOM_DATE_MINUTE := $(shell date -u +%M)
 CUSTOM_BUILD_DATE_UTC := $(shell date -d '$(CUSTOM_DATE_YEAR)-$(CUSTOM_DATE_MONTH)-$(CUSTOM_DATE_DAY) $(CUSTOM_DATE_HOUR):$(CUSTOM_DATE_MINUTE) UTC' +%s)
-CUSTOM_BUILD_DATE := $(CUSTOM_DATE_YEAR)$(CUSTOM_DATE_MONTH)$(CUSTOM_DATE_DAY)-$(CUSTOM_DATE_HOUR)$(CUSTOM_DATE_MINUTE)
+CUSTOM_BUILD_DATE := $(CUSTOM_DATE_YEAR)$(CUSTOM_DATE_MONTH)$(CUSTOM_DATE_DAY)
 
 CUSTOM_PLATFORM_VERSION := 10.0
+FLUID_NUM_VERSION := 0.6
+FLUID_CODENAME := Quenol
 
 TARGET_PRODUCT_SHORT := $(subst aosp_,,$(CUSTOM_BUILD))
 
-CUSTOM_VERSION := PixelExperience_$(CUSTOM_BUILD)-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)-$(CUSTOM_BUILD_TYPE)
+FLUID_VERSION := $(FLUID_NUM_VERSION)-$(FLUID_CODENAME)
+CUSTOM_VERSION := Fluid-$(FLUID_VERSION)-$(CUSTOM_BUILD_TYPE)-$(CUSTOM_BUILD)-$(CUSTOM_BUILD_DATE)
 CUSTOM_VERSION_PROP := ten
 
 CUSTOM_PROPERTIES := \
-    org.pixelexperience.version=$(CUSTOM_VERSION_PROP) \
-    org.pixelexperience.version.display=$(CUSTOM_VERSION) \
-    org.pixelexperience.build_date=$(CUSTOM_BUILD_DATE) \
-    org.pixelexperience.build_date_utc=$(CUSTOM_BUILD_DATE_UTC) \
-    org.pixelexperience.build_type=$(CUSTOM_BUILD_TYPE)
+    com.fluid.version=$(CUSTOM_VERSION_PROP) \
+    com.fluid.version.display=$(CUSTOM_VERSION) \
+    com.fluid.build_date=$(CUSTOM_BUILD_DATE) \
+    com.fluid.build_date_utc=$(CUSTOM_BUILD_DATE_UTC) \
+    ro.fluid.buildtype=$(CUSTOM_BUILD_TYPE) \
+    ro.fluid.build.version=$(FLUID_VERSION)
