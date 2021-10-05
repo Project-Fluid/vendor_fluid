66dbd147fa23b6e94dc1a1f0fe46c3feb25fa3cb
Michael Bestas <mkbestas@lineageos.org>
2021-10-05 23:37:55 +0300

    build: Replace obsolete ADDITIONAL_BUILD_PROPERTIES
    Fixes:
    error: ADDITIONAL_BUILD_PROPERTIES is obsolete.  Please use ADDITIONAL_SYSTEM_PROPERTIES.
    Change-Id: I7eb3c47d9fd705aa8f8d39e10563e47ffbf2d560

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,11 +1,11 @@
 # Build fingerprint
 ifneq ($(BUILD_FINGERPRINT),)
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 endif
 
 # Fluid Platform Display Version
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.fluid.display.version=$(FLUID_DISPLAY_VERSION) \
     ro.fluid.build.version=$(FLUID_BUILD_VERSION) \
     ro.fluid.build.date=$(BUILD_DATE) \
