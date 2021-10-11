c82017713af5bcd4d269410db5d6c1714245bf90
Michael Bestas <mkbestas@lineageos.org>
2021-10-11 18:31:06 +0300

    Remove color font and icon overlays
    These are not used anymore with the new theming
    
    Change-Id: Iacb086f9dacc36fca5c9a9dedcbd2b2ba4f86e6e

--- config/common_full.mk
+++ config/common_full.mk
@@ -3,15 +3,6 @@ $(call inherit-product, vendor/fluid/config/common_mobile.mk)
 
 PRODUCT_SIZE := full
 
-# Include {Lato,Rubik} fonts
-$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
-$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
-
-# Fonts
-PRODUCT_PACKAGES += \
-    LineageLatoFont \
-    LineageRubikFont
-
 # Recorder
 PRODUCT_PACKAGES += \
     Recorder
