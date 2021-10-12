LineageOS/android_vendor_lineage

Add generic targets for automotive
Makefiles combining core lineage and automotive packages.
Can be included by devices to turn them into Automotive builds.
Emulator targets demonstrate using these makefiles and form a basis for Automotive GSIs.
Change-Id: Ib5d35dfe4de8c459e4e14a7ba6f0b0e682e0d912

build/target/product/AndroidProducts.mk
@@ -26,7 +26,9 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_tv_arm.mk \
     $(LOCAL_DIR)/lineage_tv_arm64.mk \
     $(LOCAL_DIR)/lineage_tv_x86.mk \
-    $(LOCAL_DIR)/lineage_tv_x86_64.mk
+    $(LOCAL_DIR)/lineage_tv_x86_64.mk \
+    $(LOCAL_DIR)/lineage_car_arm64.mk \
+    $(LOCAL_DIR)/lineage_car_x86_64.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_arm-userdebug \
@@ -40,4 +42,6 @@ COMMON_LUNCH_CHOICES := \
     lineage_tv_arm-userdebug \
     lineage_tv_arm64-userdebug \
     lineage_tv_x86-userdebug \
-    lineage_tv_x86_64-userdebug
+    lineage_tv_x86_64-userdebug \
+    lineage_car_arm64-userdebug \
+    lineage_car_x86_64-userdebug

Project-Fluid/vendor_fluid

Change-Id: I8f572814e4c5c0c41e4f950c0a086ae3966a83de

build/target/product/AndroidProducts.mk
@@ -26,7 +26,9 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/fluid_tv_arm.mk \
     $(LOCAL_DIR)/fluid_tv_arm64.mk \
     $(LOCAL_DIR)/fluid_tv_x86.mk \
-    $(LOCAL_DIR)/fluid_tv_x86_64.mk
+    $(LOCAL_DIR)/fluid_tv_x86_64.mk \
+    $(LOCAL_DIR)/fluid_car_arm64.mk \
+    $(LOCAL_DIR)/fluid_car_x86_64.mk
 
 COMMON_LUNCH_CHOICES := \
     fluid_arm-userdebug \
@@ -40,4 +42,6 @@ COMMON_LUNCH_CHOICES := \
     fluid_tv_arm-userdebug \
     fluid_tv_arm64-userdebug \
     fluid_tv_x86-userdebug \
-    fluid_tv_x86_64-userdebug
+    fluid_tv_x86_64-userdebug \
+    fluid_car_arm64-userdebug \
+    fluid_car_x86_64-userdebug