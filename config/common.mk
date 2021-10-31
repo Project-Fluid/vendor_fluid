Havoc-OS/android_vendor_havoc

vendor: config: DEVICE_PACKAGE_OVERLAYS -> PRODUCT_PACKAGE_OVERLAYS
 * DEVICE_PACKAGE_OVERLAYS corresponds to overlays that
   are device specific, these are not so they should
   belong in platform.

Change-Id: I8684e10a370ba1b9f0a725c0f199d167a98d1668
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

config/common.mk
@@ -216,7 +216,7 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 
 # Overlays
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/havoc/overlay
-DEVICE_PACKAGE_OVERLAYS += vendor/havoc/overlay/common
+PRODUCT_PACKAGE_OVERLAYS += vendor/havoc/overlay/common
 
 # Enable ccache
 USE_CCACHE := true

Project-Fluid/vendor_fluid

config/common.mk
@@ -107,7 +107,7 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/fluid/overlay
-DEVICE_PACKAGE_OVERLAYS += vendor/fluid/overlay/common
+PRODUCT_PACKAGE_OVERLAYS += vendor/fluid/overlay/common
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/fluid/config/partner_gms.mk